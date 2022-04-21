afc5059266643bc3e4a82766a362eed29e19179f
B_Yoda04 <gerardmatos1990@gmail.com>
2022-04-21 00:01:04 -0500

    change in menu desktop and bag shopping

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const toggle = document.getElementById('toggle')
 const menu = document.getElementById('menu')
 const bag = document.getElementById('bag')
 const bagShopping = document.getElementById('bag-shopping')
-const close = document.getElementById('close')
-const counter = document.getElementById('counter')
+const cerrar = document.getElementById('close')
+const cardProduct = document.getElementById('card-product')
+const remover = document.getElementById('remove')
+const cardsProducts = document.getElementById('cards-products')
+const showAll = document.getElementById('show-all')
 
 toggle.onclick = () => {
     menu.classList.toggle('active')
@@ -15,6 +18,14 @@ bag.onclick = () => {
 }
 
 
-close.onclick = () => {
+cerrar.onclick = () => {
     bagShopping.classList.remove('active')
+}
+
+remover.onclick = () => {
+    cardProduct.classList.add('disable')
+}
+
+showAll.onclick = () => {
+    cardsProducts.classList.add('disable')
 }
